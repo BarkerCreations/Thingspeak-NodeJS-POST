7d6dbb29e65ffa185ad16c0d4933194e3933e3c8
BarkerCreations <chris.bark3r@gmail.com>
2016-09-19 11:55:07 +0100

    Initial js commit

---
+++ thingspeak.js
@@ -0,0 +1,23 @@
+var serialport = require('serialport');
+var request = require('request');
+
+var API_KEY = 'MYAPIKEYXXXXYYY'; // ThingSpeak 'write' API key
+
+/**
+ * Data needs to be a JSON object with
+ * the format field1:value, field2:value
+ * and so on for as many data points you wish
+ * to update at once.
+ * @param data
+ */
+function uploadData(data) {
+    request.post(
+        'https://api.thingspeak.com/update',
+        { json: data, api_key:API_KEY },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body)
+            }
+        }
+    );
+}
